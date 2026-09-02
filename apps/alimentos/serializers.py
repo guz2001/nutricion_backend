cb223d2f1463059a1627ad576eda33ee402376ab
guz2001 <guz20dgc@gmail.com>
2026-09-02 00:14:02 -0500

    Configuracion de modulo serialerz 2

--- apps/alimentos/serializers.py
+++ apps/alimentos/serializers.py
@@ -1,8 +1,22 @@
-from django.db import models
 from rest_framework import serializers
 from apps.alimentos.models import Alimento
+from apps.alimentos.models import GrupoAlimento
 
-class GrupoAlimentoSerializer(serializers.Models):
+class GrupoAlimentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=GrupoAlimento
+        fields=['id','nombre','descripcion']
+
+
+
+
+
+
+class AlimentoSerializer(serializers.ModelSerializer):
+
     class Meta:
         model=Alimento
-        fields=
+
+        fields=['id','grupo','subgrupo','nombre','poblacion','porcion_g','unidad_medida','kcal',
+            'proteina_g','grasa_total_g','ags_g','agm_g','agp_g','colesterol_mg','cho_g',
+            'fibra_g','calcio_mg','fosforo_mg','hierro_mg','sodio_mg']
