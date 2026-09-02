e163aae5201a70d00680810ade766114754cb887
guz2001 <guz20dgc@gmail.com>
2026-09-01 22:58:02 -0500

    Documentacion en modulo de models

--- apps/alimentos/apps.py
+++ apps/alimentos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AlimentosConfig(AppConfig):
+class AlimentoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.alimentos'
