91b1aa985ec0cf0c25b2099054a40dd338e433ee
guz2001 <guz20dgc@gmail.com>
2026-09-01 21:12:09 -0500

    configuracion archivo models.py

--- apps/alimentos/models_generados.py
+++ apps/alimentos/models_generados.py
@@ -6,8 +6,8 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-
+#TRADUCCION:esto viene apagado por defecto porque detecté una tabla existente — bórralo solo si quieres que Django la administre
+#Sale este mensaje por que detecto que ya hay una tabla existente
 class Alimentos(models.Model):
     grupo = models.ForeignKey('GruposAlimentos', models.DO_NOTHING)
     subgrupo = models.CharField(max_length=150, blank=True, null=True)
