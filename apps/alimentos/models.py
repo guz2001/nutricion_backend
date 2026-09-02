4abbabe4eefd58debb07983af165266d6d3c4bbc
guz2001 <guz20dgc@gmail.com>
2026-09-01 21:25:02 -0500

    configuracion archivo models.py

--- apps/alimentos/models.py
+++ apps/alimentos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class GrupoAlimento(models.Model):#s el nombre del grupo, como "Frutas". Cada nombre debe ser único, por eso dice unique=True.s
+    nombre=models.CharField(max_length=100,unique=True)
+    descripcion=models.TextField(blank=True,null=True)
+
+    class Meta:
+        managed=False
+        db_table='grupos_alimentos'
+
+    def __str__(self):
+        return self.nombre
+class Alimento(models.Model):
+    POBLACION_CHOICES = []
