76f66eecd600237859df27280af56465fd410b0c
guz2001 <guz20dgc@gmail.com>
2026-09-02 00:02:29 -0500

    Configuracion de modulo serialerz 2

--- apps/alimentos/serializers.py
+++ apps/alimentos/serializers.py
@@ -1,3 +1,8 @@
-
+from django.db import models
 from rest_framework import serializers
+from apps.alimentos.models import Alimento
 
+class GrupoAlimentoSerializer(serializers.Models):
+    class Meta:
+        model=Alimento
+        fields=
