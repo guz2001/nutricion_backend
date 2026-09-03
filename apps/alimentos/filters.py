79b0ba80ab0c626a4c578fa7897f72d6ae791a1a
guz2001 <guz20dgc@gmail.com>
2026-09-02 22:25:32 -0500

    Creacion filtro de grupo para grupo_id en modulo filters.py

---
+++ apps/alimentos/filters.py
@@ -0,0 +1,14 @@
+import django_filters
+from apps.alimentos.models import Alimento
+
+class AlimentoFilter(django_filters.FilterSet):
+    grupo_id= django_filters.NumberFilter(field_name='grupo') 
+    """
+    → esto crea un filtro nuevo que se llama grupo_id (así es como aparecerá en la URL: ?grupo_id=1),
+      pero por dentro, cuando ejecute la consulta, va a buscar en el campo real del modelo que es grupo 
+      (field_name='grupo' es la traducción entre el nombre de la URL y 
+      el nombre real de la columna).
+    """
+    class Meta:
+        model = Alimento
+        fields =  ['poblacion','grupo']
--- apps/alimentos/views.py
+++ apps/alimentos/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from  apps.alimentos.serializers import GrupoAlimentoSerializer,AlimentoListSerializer,AlimentoAllSerializer
 from apps.alimentos.models import GrupoAlimento,Alimento
+from apps.alimentos.filters import AlimentoFilter
 
 
 class GrupoAlimentoViewSet(viewsets.ModelViewSet):
@@ -12,6 +13,7 @@ class GrupoAlimentoViewSet(viewsets.ModelViewSet):
 class AlimentoViewSet(viewsets.ModelViewSet):
     queryset=Alimento.objects.all()
     serializer_class=AlimentoAllSerializer
+    filterset_class= AlimentoFilter
 
     def get_serializer_class(self):     # <- La lógica que necesitas es: "si la acción es 'list', usa el serializer corto(List); si no, usa el completo(All"
         if self.action == 'list':
