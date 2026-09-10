0addacfd48c0cccdf2c56b83e3d56a55dac3635d
guz2001 <guz20dgc@gmail.com>
2026-09-09 19:27:17 -0500

    Cambios filtro de poblaciones para solo visualizar en el frontend los campos con valor

--- apps/alimentos/views.py
+++ apps/alimentos/views.py
@@ -8,6 +8,16 @@ from django.db.models import Q
 class GrupoAlimentoViewSet(viewsets.ModelViewSet):
     queryset= GrupoAlimento.objects.all()
     serializer_class=GrupoAlimentoSerializer
+    
+    def get_queryset(self):
+        queryset= GrupoAlimento.objects.all()
+        poblacion=self.request.query_params.get('poblacion')
+        
+        
+        if poblacion:
+            queryset=queryset.filter(alimento__poblacion=poblacion).distinct()
+
+        return queryset
 
 class AlimentoViewSet(viewsets.ModelViewSet):
     queryset=Alimento.objects.all()
@@ -38,24 +48,15 @@ class AlimentoViewSet(viewsets.ModelViewSet):
         queryset=Alimento.objects.all()
         poblacion=self.request.query_params.get('poblacion')
         q=self.request.query_params.get('q')#Busca en el proyecto una variable con este nombre,
-        if poblacion== 'adultos':
-            queryset=queryset.filter(Q(poblacion='adultos')|Q(poblacion='niños_y_adultos'))
+        if poblacion:
+            queryset=queryset.filter(poblacion=poblacion).distinct()
             
         
-        elif poblacion== 'niños':
-            queryset=queryset.filter(Q(poblacion='niños')|Q(poblacion='niños_y_adultos'))
 
-        elif poblacion== 'menores_de_dos_anios':
-                    queryset=queryset.filter(poblacion='menores_de_dos_anios')
-
-
-        elif poblacion== 'niños_y_adultos':
-                    queryset=queryset.filter(poblacion='niños_y_adultos')
         if q:
             queryset=queryset.annotate(parecido=TrigramSimilarity('nombre',q)) #Calcular el puntaje de similitud hasta aca solo hace esto lo de abajo lo mostrar
             queryset =queryset.filter(parecido__gt=0.1)#Aca si lo  muestra por que le dice que dice que queryset es igual a solo mostrar los datos mayores a 0.1
             orderby=queryset.order_by('-parecido') 
             return orderby      # <- devuelve la lista de alimentos filtrados
-
 
         return queryset  #<- Si esta vacio devuelve todo los datos osea Alimento.objetcs.all()
